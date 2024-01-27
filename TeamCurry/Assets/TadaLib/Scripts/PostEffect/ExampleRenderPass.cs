2fde4c96b4df86849f1b47b824c5d9655279382b
tada0389 <tatata0389@gmail.com>
2024-01-27 11:29:41 +0900

    fix: reimport TadaLib

---
+++ TeamCurry/Assets/TadaLib/Scripts/PostEffect/ExampleRenderPass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public sealed class ExampleRenderPass : ScriptableRenderPass
+{
+    private const string RenderPassName = nameof(ExampleRenderPass);
+    private readonly Material _material;
+
+    public ExampleRenderPass(Shader shader)
+    {
+        if (shader == null)
+        {
+            return;
+        }
+
+        _material = new Material(shader);
+        renderPassEvent = RenderPassEvent.AfterRenderingPostProcessing;
+    }
+
+    public override void Execute(ScriptableRenderContext context, ref RenderingData data)
+    {
+        if (_material == null)
+        {
+            return;
+        }
+
+        var cmd = CommandBufferPool.Get(RenderPassName);
+
+        // 一回Blitするだけでカラーバッファにマテリアルが適用される
+        Blit(cmd, ref data, _material);
+
+        context.ExecuteCommandBuffer(cmd);
+        CommandBufferPool.Release(cmd);
+    }
+}
