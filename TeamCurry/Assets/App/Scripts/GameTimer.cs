f2d4d64a5e6a15d162eb2f8179aee7c21374de0b
AuroraSloan <lanajadethompson@gmail.com>
2024-01-27 22:32:09 +0900

    timer guard move

--- TeamCurry/Assets/App/Scripts/GameTimer.cs
+++ TeamCurry/Assets/App/Scripts/GameTimer.cs
@@ -1,21 +1,34 @@
+using System;
 using UnityEngine;
 
 public class GameTimer : MonoBehaviour
 {
+    [SerializeField] private Transform guard;
+    [SerializeField] private Transform guardStart;
+    [SerializeField] private Transform guardEnd;
     [SerializeField] private bool isTimerPaused = false;
     [SerializeField] private float maxTime = 30f;
     [SerializeField] private float time;
+    private Vector3 guardPosCreep;
+    private float prevTime;
     
     void Awake()
     {
         time = maxTime;
+        guardPosCreep = Vector3.zero;
     }
     
     void Update()
     {
         if (!TimerDone() && !isTimerPaused)
         {
-            time -= Time.deltaTime;    
+            time -= Time.deltaTime;
+            float timeElapsedRatio = (maxTime - time) / maxTime;
+            float totalDistance = Vector3.Distance(guardStart.position, guardEnd.position);
+            Vector3 guardDirection = guardEnd.position - guardStart.position;
+            guardDirection.Normalize();
+            guardDirection *= (timeElapsedRatio * totalDistance);
+            guard.position = guardDirection + guardStart.position;
         }
     }
 
@@ -26,6 +39,7 @@ public class GameTimer : MonoBehaviour
     
     public void SetTimer(float timeLimit)
     {
+        maxTime = timeLimit;
         time = timeLimit;
     }
 
