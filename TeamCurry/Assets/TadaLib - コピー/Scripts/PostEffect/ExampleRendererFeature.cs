077ae8776e415ae78ebe1f296f3680c5d52b46db
tada0389 <tatata0389@gmail.com>
2024-01-27 11:20:01 +0900

    fix: delete TadaLib

---
+++ TeamCurry/Assets/TadaLib - コピー/Scripts/PostEffect/ExampleRendererFeature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+[Serializable]
+public sealed class ExampleRendererFeature : ScriptableRendererFeature
+{
+    [SerializeField] private Shader _shader;
+
+    private ExampleRenderPass _postProcessPass;
+
+    public override void Create()
+    {
+        _postProcessPass = new ExampleRenderPass(_shader);
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        if (_postProcessPass != null)
+        {
+            renderer.EnqueuePass(_postProcessPass);
+        }
+    }
+}
