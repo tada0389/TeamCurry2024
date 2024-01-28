47e86a08b268c549d4571d35a00126032d8611be
Joseph Arneil <josepharneil@gmail.com>
2024-01-28 15:31:54 +0900

    Vase stage resetting

--- TeamCurry/Assets/App/Scripts/VaseStage.cs
+++ TeamCurry/Assets/App/Scripts/VaseStage.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public class VaseStage : Stage
 {
     [SerializeField] private Transform thiefTransform;
-    [SerializeField] private GameTimer timer;
+    [SerializeField] private GameTimer gameTimer;
+    [SerializeField] private App.Actor.Player.MoveCtrl playerMoveCtrl;
+    [SerializeField] private Transform playerStartPos;
 
     private void Awake()
     {
@@ -13,11 +15,20 @@ public class VaseStage : Stage
 
     public override StagePhaseState Setup()
     {
+        gameTimer.SetTimer(this.timeLimit);
+        gameTimer.PauseTimer();
+        this.StageOutcome = StageOutcome.Undefined;
+        guard.Reset();
+        thiefTransform.position = playerStartPos.position;
+
+        playerMoveCtrl.enabled = true;
+        this.gameObject.SetActive(true);
         return StagePhaseState.Done;
     }
 
     public override StagePhaseState StartStage()
     {
+        gameTimer.StartTimer();
         return StagePhaseState.Done;
     }
 
@@ -34,17 +45,20 @@ public class VaseStage : Stage
              return StagePhaseState.Done;           
         }
 
-        return timer.TimerDone() ? StagePhaseState.Done : StagePhaseState.Active;
+        return gameTimer.TimerDone() ? StagePhaseState.Done : StagePhaseState.Active;
     }
 
     public override StagePhaseState End()
     {
-        this.gameObject.SetActive(false);
         return StagePhaseState.Done;
     }
 
     public override StagePhaseState Shutdown()
     {
+        this.gameObject.SetActive(false);
+        playerMoveCtrl.enabled = false;
+        guard.Reset();
+        gameTimer.PauseTimer();
         return StagePhaseState.Done;
     }
 }
